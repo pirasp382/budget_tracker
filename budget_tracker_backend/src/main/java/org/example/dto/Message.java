11ce9cbc0709d36615f80c9029657e0bcdde2639
Tim Deimel <t.deimel@teckpro.de>
2025-02-22 15:14:29 +0100

    edit Message Dto

--- budget_tracker_backend/src/main/java/org/example/dto/Message.java
+++ budget_tracker_backend/src/main/java/org/example/dto/Message.java
@@ -4,11 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Message {
+
+  @Schema(
+          description = "title of the Message",
+          implementation = String.class,
+          examples = {"Username is null"}
+  )
   private String title;
 }
