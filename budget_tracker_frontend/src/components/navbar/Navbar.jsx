b298a5884375e35812e642ff8f34e830cad7b5c5
Tim Deimel <t.deimel@teckpro.de>
2025-03-01 15:45:41 +0100

    edit navbar, add all site links

--- budget_tracker_frontend/src/components/navbar/Navbar.jsx
+++ budget_tracker_frontend/src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import "./Navbar.css"
 import {useNavigate} from "react-router-dom"
 import {IoIosLogOut} from "react-icons/io"
 
-const Navbar = () => {
+const Navbar = ({site}) => {
     const navigate = useNavigate()
 
     const logout = () => {
@@ -13,11 +13,11 @@ const Navbar = () => {
     return (
         <div className={"navbar-items"}>
             <ul className={"navbar-menu"}>
-                <li className={"menu-item"} onClick={() => navigate("/")}>Overview</li>
-                <li className={"menu-item"} onClick={() => navigate("/transactions")}>Transactions</li>
-                <li className={"menu-item"} onClick={() => navigate("/accounts")}>Accounts</li>
-                <li className={"menu-item"} onClick={() => navigate("/categories")}>Categories</li>
-                <li className={"menu-item"} onClick={() => navigate("/settings")}>Settings</li>
+                <li className={`menu-item ${site === "dashboard" ? "active" : ""}`} onClick={() => navigate("/")}>Overview</li>
+                <li className={`menu-item ${site === "transactions" ? "active" : ""}`} onClick={() => navigate("/transactions")}>Transactions</li>
+                <li className={`menu-item ${site === "accounts" ? "active" : ""}`} onClick={() => navigate("/accounts")}>Accounts</li>
+                <li className={`menu-item ${site === "categories" ? "active" : ""}`} onClick={() => navigate("/categories")}>Categories</li>
+                <li className={`menu-item ${site === "settings" ? "active" : ""}`} onClick={() => navigate("/settings")}>Settings</li>
             </ul>
             <div className={"logout"}>
                 <IoIosLogOut className={"logout-icon"} onClick={() => logout()}/>
