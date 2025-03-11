0bb3da6bec405e76eb95ce7c8574e0bf0bbeee11
Tim Deimel <t.deimel@teckpro.de>
2025-03-11 16:15:15 +0100

    add account filter

--- budget_tracker_frontend/src/components/header/Header.jsx
+++ budget_tracker_frontend/src/components/header/Header.jsx
@@ -2,8 +2,31 @@ import "./Header.css"
 import Logo from "../logo/Logo"
 import PropTypes from "prop-types"
 import Navbar from "../navbar/Navbar"
+import {useDispatch, useSelector} from "react-redux"
+import {useEffect, useState} from "react"
+import {setFilterAccount} from "../../redux/actions"
+import "react-datepicker/dist/react-datepicker.css"
+
+const today = () => new Date()
 
 const Header = ({username, site}) => {
+
+    const dispatch = useDispatch()
+    const [accounts, setAccounts] = useState([])
+    const filter = useSelector((state) => state.accounts.filter_account)
+
+    const handleFilteredAccount = (e) => {
+        dispatch(setFilterAccount(e))
+    }
+
+    const resetFilter = () => {
+        dispatch(setFilterAccount(""))
+    }
+
+    useEffect(() => {
+        setAccounts(JSON.parse(localStorage.getItem("accounts")))
+    }, [])
+
     return (
         <div className={"header"}>
             <div className={"navbar"}>
@@ -15,7 +38,21 @@ const Header = ({username, site}) => {
                     Welcome Back, {username} 👋
                 </div>
                 <div className={"sub-title"}>
-                    This is your financial Overiew Report
+                    This is your financial Overview Report
+                </div>
+                <div className={"account-list-header"}>
+                    <div>
+                        <select value={filter} onChange={event => {
+                            handleFilteredAccount(event.target.value)
+                        }}>
+                            <option value="" disabled selected>Select your Account</option>
+                            {accounts.map(item => (
+                                <option value={item.id}>{item.title}</option>))}
+                        </select>
+                        <div className={"buttons"}>
+                            <button onClick={resetFilter}>reset filter</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -24,6 +61,7 @@ const Header = ({username, site}) => {
 
 Header.propTypes = {
     username: PropTypes.string,
+    site: PropTypes.string,
 }
 
 export default Header
